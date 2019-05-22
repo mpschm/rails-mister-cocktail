7efdd671e6fcf9cb6efda1ce05fc73b7f476ae92
Marie Pierre Schmitz mpschmitz@hotmail.fr <mpschmitz@hotmail.fr>
2019-05-22 12:29:41 +0200

    master

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
+  root to: 'articles#index'
 
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create]
